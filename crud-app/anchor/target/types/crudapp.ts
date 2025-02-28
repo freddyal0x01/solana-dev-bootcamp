42178e37125a52de1b43ad64809d063bd164103c
freddyal <freddyal0x01@gmail.com>
2025-02-28 14:53:15 -0500

    Created a CRUD app in Solana

--- crud-app/anchor/target/types/crudapp.ts
+++ crud-app/anchor/target/types/crudapp.ts
@@ -5,196 +5,160 @@
  * IDL can be found at `target/idl/crudapp.json`.
  */
 export type Crudapp = {
-  "address": "coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF",
-  "metadata": {
-    "name": "crudapp",
-    "version": "0.1.0",
-    "spec": "0.1.0",
-    "description": "Created with Anchor"
-  },
-  "instructions": [
+  address: "Fu1bF3LB9d5wMNDFmoBUgWYsq9Em1EKRH3XAQFh6QfSx";
+  metadata: {
+    name: "crudapp";
+    version: "0.1.0";
+    spec: "0.1.0";
+    description: "Created with Anchor";
+  };
+  instructions: [
     {
-      "name": "createJournalEntry",
-      "discriminator": [
-        48,
-        65,
-        201,
-        186,
-        25,
-        41,
-        127,
-        0
-      ],
-      "accounts": [
+      name: "createJournalEntry";
+      discriminator: [48, 65, 201, 186, 25, 41, 127, 0];
+      accounts: [
         {
-          "name": "journalEntry",
-          "writable": true,
-          "pda": {
-            "seeds": [
+          name: "journalEntry";
+          writable: true;
+          pda: {
+            seeds: [
               {
-                "kind": "arg",
-                "path": "title"
+                kind: "arg";
+                path: "title";
               },
               {
-                "kind": "account",
-                "path": "owner"
-              }
-            ]
-          }
+                kind: "account";
+                path: "owner";
+              },
+            ];
+          };
         },
         {
-          "name": "owner",
-          "writable": true,
-          "signer": true
+          name: "owner";
+          writable: true;
+          signer: true;
         },
         {
-          "name": "systemProgram",
-          "address": "11111111111111111111111111111111"
-        }
-      ],
-      "args": [
+          name: "systemProgram";
+          address: "11111111111111111111111111111111";
+        },
+      ];
+      args: [
         {
-          "name": "title",
-          "type": "string"
+          name: "title";
+          type: "string";
         },
         {
-          "name": "message",
-          "type": "string"
-        }
-      ]
+          name: "message";
+          type: "string";
+        },
+      ];
     },
     {
-      "name": "deleteJournalEntry",
-      "discriminator": [
-        156,
-        50,
-        93,
-        5,
-        157,
-        97,
-        188,
-        114
-      ],
-      "accounts": [
+      name: "deleteJournalEntry";
+      discriminator: [156, 50, 93, 5, 157, 97, 188, 114];
+      accounts: [
         {
-          "name": "journalEntry",
-          "writable": true,
-          "pda": {
-            "seeds": [
+          name: "journalEntry";
+          writable: true;
+          pda: {
+            seeds: [
               {
-                "kind": "arg",
-                "path": "title"
+                kind: "arg";
+                path: "title";
               },
               {
-                "kind": "account",
-                "path": "owner"
-              }
-            ]
-          }
+                kind: "account";
+                path: "owner";
+              },
+            ];
+          };
         },
         {
-          "name": "owner",
-          "writable": true,
-          "signer": true
+          name: "owner";
+          writable: true;
+          signer: true;
         },
         {
-          "name": "systemProgram",
-          "address": "11111111111111111111111111111111"
-        }
-      ],
-      "args": [
+          name: "systemProgram";
+          address: "11111111111111111111111111111111";
+        },
+      ];
+      args: [
         {
-          "name": "title",
-          "type": "string"
-        }
-      ]
+          name: "title";
+          type: "string";
+        },
+      ];
     },
     {
-      "name": "updateJournalEntry",
-      "discriminator": [
-        113,
-        164,
-        49,
-        62,
-        43,
-        83,
-        194,
-        172
-      ],
-      "accounts": [
+      name: "updateJournalEntry";
+      discriminator: [113, 164, 49, 62, 43, 83, 194, 172];
+      accounts: [
         {
-          "name": "journalEntry",
-          "writable": true,
-          "pda": {
-            "seeds": [
+          name: "journalEntry";
+          writable: true;
+          pda: {
+            seeds: [
               {
-                "kind": "arg",
-                "path": "title"
+                kind: "arg";
+                path: "title";
               },
               {
-                "kind": "account",
-                "path": "owner"
-              }
-            ]
-          }
+                kind: "account";
+                path: "owner";
+              },
+            ];
+          };
         },
         {
-          "name": "owner",
-          "writable": true,
-          "signer": true
+          name: "owner";
+          writable: true;
+          signer: true;
         },
         {
-          "name": "systemProgram",
-          "address": "11111111111111111111111111111111"
-        }
-      ],
-      "args": [
+          name: "systemProgram";
+          address: "11111111111111111111111111111111";
+        },
+      ];
+      args: [
         {
-          "name": "title",
-          "type": "string"
+          name: "title";
+          type: "string";
         },
         {
-          "name": "message",
-          "type": "string"
-        }
-      ]
-    }
-  ],
-  "accounts": [
+          name: "message";
+          type: "string";
+        },
+      ];
+    },
+  ];
+  accounts: [
     {
-      "name": "journalEntryState",
-      "discriminator": [
-        113,
-        86,
-        110,
-        124,
-        140,
-        14,
-        58,
-        66
-      ]
-    }
-  ],
-  "types": [
+      name: "journalEntryState";
+      discriminator: [113, 86, 110, 124, 140, 14, 58, 66];
+    },
+  ];
+  types: [
     {
-      "name": "journalEntryState",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: "journalEntryState";
+      type: {
+        kind: "struct";
+        fields: [
           {
-            "name": "owner",
-            "type": "pubkey"
+            name: "owner";
+            type: "pubkey";
           },
           {
-            "name": "title",
-            "type": "string"
+            name: "title";
+            type: "string";
           },
           {
-            "name": "message",
-            "type": "string"
-          }
-        ]
-      }
-    }
-  ]
+            name: "message";
+            type: "string";
+          },
+        ];
+      };
+    },
+  ];
 };
